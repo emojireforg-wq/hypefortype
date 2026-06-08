b35c0adca43389b219b9fcdacddfdc102a86d968
emojireforg-wq <emojireforg@gmail.com>
2026-06-08 18:02:56 +0100

    Update licensing.js

--- pages/licensing.js
+++ pages/licensing.js
@@ -1,1 +1,8 @@
-export default function Licensing() { return (<div><h1>Licensing Information</h1><p>Select your license type to see pricing.</p></div>); }
+export default function Licensing() {
+  return (
+    <div>
+      <h1>Licensing Information</h1>
+      <p>Select your license type to see pricing.</p>
+    </div>
+  );
+}
